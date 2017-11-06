f07129a60f2da323cb2b0ab5f656207ddafc275c
Howard Feger <hfeger4@gmail.com>
2017-11-06 14:45:31 -0800

    app change to class

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyDalHtnKREbLdDrhShX3afAWSZaI5AqJW4';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+  console.log(data);
+});
+
 //create new component that produces html
 
-const App = () => {
+class App extends Component{
+  render(){
   return (
     <div>
       <SearchBar />
     </div>
   );
-};
+  }
+}
 
 //take this components generated html and put it in
 //on the page (in the dom)
-
 ReactDOM.render(<App />, document.querySelector('.container'));
