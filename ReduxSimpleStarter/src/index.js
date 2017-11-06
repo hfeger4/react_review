d4056b464daa26265773114472bef4e3260279f3
Howard Feger <hfeger4@gmail.com>
2017-11-06 15:08:50 -0800

    add video_list

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -2,19 +2,28 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDalHtnKREbLdDrhShX3afAWSZaI5AqJW4';
-
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-  console.log(data);
-});
 
 //create new component that produces html
 
 class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({videos});
+      //this.setState({videos: videos})
+    });
+
+  }
   render(){
   return (
     <div>
       <SearchBar />
+      <VideoList videos={this.state.videos}/>
     </div>
   );
   }
