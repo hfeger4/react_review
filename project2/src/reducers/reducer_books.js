5c24966a9113cd035e75688f63dda46dcc68e744
Howard Feger <hfeger4@gmail.com>
2017-11-09 11:46:43 -0800

    add book reducer

---
+++ project2/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return [
+    { title: 'Howie is awesome'},
+    { title: 'World of Warcraft'},
+    { title: 'League of Legends'},
+    { title: 'Counter Strike GO'}
+  ];
+}
