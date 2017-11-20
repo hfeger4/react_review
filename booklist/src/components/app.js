b442eee850fac51cb63269c81ba251b6a8def536
Howard Feger <hfeger4@gmail.com>
2017-11-20 13:55:25 -0800

    add event click handler for selectBook

--- booklist/src/components/app.js
+++ booklist/src/components/app.js
@@ -4,7 +4,9 @@ import BookList from '../containers/book-list';
 export default class App extends Component {
   render() {
     return (
-      <div><BookList /></div>
+      <div>
+        <BookList />
+      </div>
     );
   }
 }
