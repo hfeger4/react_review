7d5d7ed197fd7e60799c02efaa1e9583f829079e
Howard Feger <hfeger4@gmail.com>
2017-11-21 15:42:31 -0800

    display pressure and humidity, apply CSS

--- weather_app/src/containers/weather_list.js
+++ weather_app/src/containers/weather_list.js
@@ -6,13 +6,15 @@ class WeatherList extends Component{
   renderWeather(cityData){
     const name = cityData.city.name
     const temps = cityData.list.map(weather => weather.main.temp);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
     console.log(temps);
     return(
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Chart data={temps} color="orange" />
-        </td>
+        <td><Chart data={temps} color="orange" units="K"/></td>
+        <td><Chart data={pressure} color="green" units="hpA"/></td>
+        <td><Chart data={humidity} color="black" units="%"/></td>
       </tr>
     )
   }
@@ -22,9 +24,9 @@ class WeatherList extends Component{
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
