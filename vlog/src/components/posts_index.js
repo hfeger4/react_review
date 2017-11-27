b363359e45e943a857841968cf34edb6f29bd4c8
Howard Feger <hfeger4@gmail.com>
2017-11-27 15:23:43 -0800

    fix import error

--- vlog/src/components/posts_index.js
+++ vlog/src/components/posts_index.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions'; 
 
 class PostsIndex extends Component{
+  componentDidMount(){
+    this.props.fetchPosts();
+  }
   render(){
     return (
       <div>PostsIndex</div>
@@ -8,4 +13,4 @@ class PostsIndex extends Component{
   }
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
