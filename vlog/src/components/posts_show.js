145dc1b8643c33fb89f717b5b8abc9c6013b870e
Howard Feger <hfeger4@gmail.com>
2017-11-27 18:32:16 -0800

    show individual post, fix error regarding loading

--- vlog/src/components/posts_show.js
+++ vlog/src/components/posts_show.js
@@ -9,16 +9,22 @@ class PostsShow extends Component{
   }
   render(){
     // posts[this.props.match.params.id] = post we want to show
+    const { post } = this.props
+    if (!post){
+      return <div> Loading... </div>;
+    }
     return(
       <div>
-        PostsShow
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     )
   }
 }
 
 function mapStateToProps({posts}, ownProps){
-  return {post: posts[ownProps.match.params.id]};
+  return { post: posts[ownProps.match.params.id] };
 }
 
 export default connect(mapStateToProps, {fetchPost})(PostsShow);
