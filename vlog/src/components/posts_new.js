452bedbb57e2131181a1696b6a9818ec8d7ce683
Howard Feger <hfeger4@gmail.com>
2017-11-27 17:43:53 -0800

    add callback to onSubmit, add promise to new post action

--- vlog/src/components/posts_new.js
+++ vlog/src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component{
   renderField(field){ //return some jsx, field connects it
@@ -21,7 +23,9 @@ class PostsNew extends Component{
     )
   }
   onSubmit(values){
-    console.log(values);
+    this.props.createPost(values, ()=>{
+      this.props.history.push('/');
+    });
   }
   render(){
     const { handleSubmit } = this.props;
@@ -69,4 +73,6 @@ function validate(values){
 export default reduxForm({
   validate,
   form: 'PostsNewForm' //name of form, make it unique
-})(PostsNew);
+})(
+  connect(null, {createPost})(PostsNew)
+);
