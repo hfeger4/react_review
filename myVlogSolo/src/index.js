eaea32c3b1ad012484dddb2950eec89b8e9ce39f
Howard Feger <hfeger4@gmail.com>
2017-11-29 23:07:18 -0800

    add browser, route, and switch

--- myVlogSolo/src/index.js
+++ myVlogSolo/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import promise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
