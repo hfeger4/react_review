f6e9b99902345126a5c2fcd678b74e60d30ece06
Howard Feger <hfeger4@gmail.com>
2017-12-13 17:58:04 -0800

    removed error messages

--- vlog/src/actions/index.js
+++ vlog/src/actions/index.js
@@ -15,26 +15,26 @@ export function fetchPosts(){
   return {
     type: FETCH_POSTS,
     payload: request
-  }
+  };
 }
 
 export function createPost(values, callback){
   const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
     .then(()=> callback());
-  
+
   return {
     type: CREATE_POST,
     payload: request
-  }
+  };
 }
 
 export function fetchPost(id){
-  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`)
-  
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
   return{
     type: FETCH_POST,
     payload: request
-  }
+  };
 }
 
 export function deletePost(id, callback){
@@ -43,5 +43,5 @@ export function deletePost(id, callback){
   return{
     type: DELETE_POST,
     payload: id
-  }
+  };
 }
