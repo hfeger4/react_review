7ebdcb0ff89240fc0b94935756130bce1c80da61
Howard Feger <hfeger4@gmail.com>
2018-02-28 23:57:50 -0800

    removed error message on reducer weather

--- weather_app/src/reducers/reducer_weather.js
+++ weather_app/src/reducers/reducer_weather.js
@@ -3,10 +3,10 @@ import {FETCH_WEATHER} from '../actions/index';
 export default function WeatherReducer(state=[], action){
   // console.log('Action received', action);
   switch(action.type){
-    case FETCH_WEATHER: 
-      // return state.concat([ action.payload.data ]); 
-      return [action.payload.data, ...state] //[city, city, city]
+    case FETCH_WEATHER:
+      // return state.concat([ action.payload.data ]);
+      return [action.payload.data, ...state]; //[city, city, city]
   }
-  
+
   return state;
 }
